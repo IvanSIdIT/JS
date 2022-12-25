2778ce477e5a5ebf703ccb2e5c6bc758ce91e355
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2022-12-25 23:08:44 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -31,5 +31,54 @@
 // document.getElementById('min').innerHTML = min;
 function check() {
   var min = document.getElementById('example').value;
+  if (min < 25) {
+    min = 'you are younger then 25';
+  } else if (min < 55) {
+    min = 'you are younger then 55';
+  } else if (min < 90) {
+    min = 'you are younger then 90';
+  } else {
+    min = 'type your real age or dont use any letters';
+  }
   document.getElementById('min').innerHTML = min;
 }
+
+// ДЗ №2
+// let users = '132';
+// console.log(users ?? 'Без имени');
+
+// let qwe = 0;
+// let newqwe = ++qwe;
+// console.log(newqwe);
+
+// console.log(558 > 22++);
+
+// конец ДЗ №2
+
+// ДЗ №3
+
+// let message = 'hello';
+// let FalseMessage = 'bye';
+
+// if (5 == 5) {
+//   console.log(message);
+// } else {
+//   console.log(FalseMessage);
+// }
+
+// let checks = 5 == 5 ? 'yes' : 'no';
+// console.log(checks);
+
+// let age = min
+
+// if (min < 25) {
+//   console.log('You are too young');
+// } else if (min < 55) {
+//   console.log('You are not too young');
+// } else if (min < 90) {
+//   console.log('You are  too old');
+// } else {
+//   console.log('Type your real age');
+// }
+
+// конец ДЗ №3
