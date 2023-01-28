ffec638f77eb7a5ac46aaa1adb744772f4e20c7e
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2023-01-28 23:45:17 +0200

    upd

--- JS/script1.js
+++ JS/script1.js
@@ -59,6 +59,6 @@
 
 // greet('Daniel', 'Daniel');
 
-function reverseWords(str) {
-  return str.split('').reverse().join();
-}
+// function reverseWords(str) {
+//   return str.split('').reverse().join();
+// }
