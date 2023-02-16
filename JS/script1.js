07c596b9d9aef8c79e85dba13c9623f686a861ef
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2023-02-16 22:42:32 +0200

    upd

--- JS/script1.js
+++ JS/script1.js
@@ -218,13 +218,29 @@
 
 // addToArrayForm([1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516);
 
-function sumCubes(n) {
-  let res = 0;
+// function sumCubes(n) {
+//   let res = 0;
 
-  for (let i = 0; i < n; i++) {
-    res += Math.pow(i, 3);
-  }
-  console.log(res);
-}
+//   for (let i = 0; i < n; i++) {
+//     res += Math.pow(i, 3);
+//   }
+//   console.log(res);
+// }
 
-sumCubes(2);
+// sumCubes(2);
+
+// function countPositivesSumNegatives(input) {
+//   if (input === null) return [];
+//   let count = 0;
+//   let sum = 0;
+//   for (let n of input) {
+//     if (n > 0) count += 1;
+//     if (n < 0) sum += n;
+//   }
+
+//   return [count, sum];
+// }
+
+let test = [];
+
+console.log(typeof test[0]);
