6741cc375fa39ed524b75d5a04c950457ef4d816
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2023-02-16 17:11:18 +0200

    upd

--- JS/script1.js
+++ JS/script1.js
@@ -204,6 +204,27 @@
 // }
 // console.log(validatePIN(1234));
 
-let pis = 123;
-console.log(Number.isInteger(pis));
-console.log(Math.sqrt(pis));
+// let pis = 123;
+// console.log(Number.isInteger(pis));
+// console.log(Math.sqrt(pis));
+
+// function addToArrayForm(num, k) {
+//   num = Number(num.join(''));
+//   num = num + k;
+//   console.log(Array.from(String(num)));
+//   if (num < 12630717197566440000) {
+//   }
+// }
+
+// addToArrayForm([1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516);
+
+function sumCubes(n) {
+  let res = 0;
+
+  for (let i = 0; i < n; i++) {
+    res += Math.pow(i, 3);
+  }
+  console.log(res);
+}
+
+sumCubes(2);
