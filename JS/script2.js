98539bec677e23fe7745ce779aa5e45e462ea51c
IvanSidIT <sidelnicov2006@gmail.com>
2023-03-19 16:35:03 +0200

    upd

--- JS/script2.js
+++ JS/script2.js
@@ -1,25 +1,36 @@
-let button = document.querySelector('.button');
-let elem = document.querySelector('.train__list');
-let delegate = document.querySelectorAll('.delegate');
+// let button = document.querySelector('.button');
+// let elem = document.querySelector('.train__list');
+// let delegate = document.querySelectorAll('.delegate');
 
-let script = element => {
-  if (element.target.closest('.button')) {
-    elem.classList.toggle('active');
-  }
+// let script = element => {
+//   if (element.target.closest('.button')) {
+//     elem.classList.toggle('active');
+//   }
 
-  if (!element.target.closest('.button') && !element.target.closest('.train__list')) {
-    elem.classList.remove('active');
-  }
-};
+//   if (!element.target.closest('.button') && !element.target.closest('.train__list')) {
+//     elem.classList.remove('active');
+//   }
+// };
 
-document.addEventListener('click', script);
+// document.addEventListener('click', script);
 
 //Деллегирование
 
-elem.addEventListener('click', deleg);
+// elem.addEventListener('click', deleg);
 
-function deleg(el) {
-  if (el.target.closest('.delegate')) {
-    console.log('Clicked!');
-  }
+// function deleg(el) {
+//   if (el.target.closest('.delegate')) {
+//     console.log('Clicked!');
+//   }
+// }
+
+const field = document.getElementById('field');
+const ball = document.getElementById('ball');
+
+function ballMove(event) {
+  console.log(event.clientX, event.clientY);
+  ball.style.left = event.clientX;
+  ball.style.top = event.clientY;
 }
+
+field.addEventListener('click', ballMove);
