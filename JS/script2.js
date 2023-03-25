11665e3de532dd67fd29f9580250a2aca2bd32af
IvanSidIT <sidelnicov2006@gmail.com>
2023-03-25 21:30:08 +0200

    upd

--- JS/script2.js
+++ JS/script2.js
@@ -25,22 +25,23 @@
 // }
 
 let border = Math.abs(
+  //Размер  границы поля
   document.getElementById('field').clientWidth - document.getElementById('field').offsetWidth
 );
 
-const field = document.getElementById('field');
-const ball = document.getElementById('ball');
+const field = document.getElementById('field'); //Переменная с полем
+const ball = document.getElementById('ball'); //Переменная с мячем
 
 function ballMove(event) {
   if (event.clientX > 30) {
-    ball.style.marginLeft = `${event.clientX - border / 2}px`;
+    ball.style.marginLeft = `${event.clientX - border / 2}px`; //Задаем левый марджин
     console.log(event.clientX - border / 2);
   } else {
     ball.style.marginLeft = `${ball.offsetWidth / 2}px`;
   }
 
   if (event.clientY > 30) {
-    ball.style.marginTop = `${event.clientY - border / 2}px`;
+    ball.style.marginTop = `${event.clientY - border / 2}px`; //Задаем марджин топ
   } else {
     ball.style.marginTop = `${ball.offsetWidth / 2}px`;
   }
