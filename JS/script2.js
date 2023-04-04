75912dfdcfc054d7215ca49547cbca03b7346c61
Ivan <sidelnicov2006@gmail.com>
2023-04-04 13:31:01 +0300

    upd

--- JS/script2.js
+++ JS/script2.js
@@ -24,31 +24,6 @@
 //   }
 // }
 
-let border = Math.abs(
-  //Размер  границы поля
-  document.getElementById('field').clientWidth - document.getElementById('field').offsetWidth
-);
-
-const field = document.getElementById('field'); //Переменная с полем
-const ball = document.getElementById('ball'); //Переменная с мячем
-
-function ballMove(event) {
-  if (event.clientX > 30) {
-    ball.style.marginLeft = `${event.clientX - border / 2}px`; //Задаем левый марджин
-    console.log(event.clientX - border / 2);
-  } else {
-    ball.style.marginLeft = `${ball.offsetWidth / 2}px`;
-  }
-
-  if (event.clientY > 30) {
-    ball.style.marginTop = `${event.clientY - border / 2}px`; //Задаем марджин топ
-  } else {
-    ball.style.marginTop = `${ball.offsetWidth / 2}px`;
-  }
-}
-
-field.addEventListener('click', ballMove); //Вешаем слушатель
-
 // Events
 
 const block = document.querySelector('.events');
